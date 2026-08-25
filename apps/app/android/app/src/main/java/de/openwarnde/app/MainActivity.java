72f9a215e24d3b3aba801b55eae786e5817afe93
Florian Cramer <94528595+FlorianCramer@users.noreply.github.com>
2026-08-25 17:01:24 +0000

    [#4] - add capacitor to app

---
+++ apps/app/android/app/src/main/java/de/openwarnde/app/MainActivity.java
@@ -0,0 +1,5 @@
+package de.openwarnde.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   allowedDevOrigins: ["127.0.0.1"],
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
